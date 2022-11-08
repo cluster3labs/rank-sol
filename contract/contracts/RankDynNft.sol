f965778f2cba275da2261f0493f94e4826fef490
limy <whee@zlext.com>
2022-11-08 15:05:44 +0800

    fix response get

--- contract/contracts/RankDynNft.sol
+++ contract/contracts/RankDynNft.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import '@chainlink/contracts/src/v0.8/ChainlinkClient.sol';
 import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
 
-contract RankDynNft is ERC1155,ChainlinkClient {
+contract RankDynNft is ERC1155, ChainlinkClient {
     using Chainlink for Chainlink.Request;
 
     uint256 public volume;
@@ -21,14 +21,15 @@ contract RankDynNft is ERC1155,ChainlinkClient {
 
     mapping(address => uint256) balanceMapping;
 
-    event RequestVolume(bytes32 indexed requestId, uint256 volume);
+    event GetResponse(bytes32 indexed requestId, bytes volume);
 
     constructor() ERC1155("http://rank.flowingcloud.cn/openapi/token/{id}.json") {
         owner = msg.sender;
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
         setChainlinkOracle(0x40193c8518BB267228Fc409a613bDbD8eC5a97b3);
         jobId = '7da2702f37fd48e5b1b9a5715e3509b6';
-        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+        fee = (1 * LINK_DIVISIBILITY) / 10;
+        // 0,1 * 10**18 (Varies by network and job)
     }
 
     modifier onlyOwner() {
@@ -54,21 +55,15 @@ contract RankDynNft is ERC1155,ChainlinkClient {
         return balanceMapping[account];
     }
 
-    function updateRankUrl() public onlyOwner {
-        string memory rankUrlNew = requestVolumeData();
-        rankUrl = rankUrlNew;
+    function updateRankUrlRequest() public onlyOwner {
+        request();
     }
 
     /**
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function requestVolumeData() public returns (string memory) {
-        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-
-        // Set the URL to perform the GET request on
-        req.add('get', 'https://rank.flowingcloud.cn/openapi/token/rankUrl');
-
+    function request() public {
         // Set the path to find the desired data in the API response, where the response format is:
         // {"RAW":
         //   {"ETH":
@@ -80,28 +75,37 @@ contract RankDynNft is ERC1155,ChainlinkClient {
         //   }
         //  }
         // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
-        req.add('path', 'data'); // Chainlink nodes 1.0.0 and later support this format
+        // Chainlink nodes 1.0.0 and later support this format
 
-        // Multiply the result by 1000000000000000000 to remove decimals
-        //        int256 timesAmount = 10**18;
-        //        req.addInt('times', timesAmount);
-
-        // Sends the request
-        bytes32 chainLink = sendChainlinkRequest(req, fee);
-        return bytes32ToString(chainLink);
+        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        req.add(
+            'get',
+            'https://rank.flowingcloud.cn/openapi/token/rankUrl'
+        );
+        req.add('path', 'data');
+        sendChainlinkRequest(req, (1 * LINK_DIVISIBILITY) / 10);
+        // 0,1*10**18 LINK
     }
 
+    bytes public data;
     /**
-     * Receive the response in the form of uint256
+     * Receive the response in the form of bytes32
      */
-    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId) {
-        emit RequestVolume(_requestId, _volume);
-        volume = _volume;
+    function fulfill(bytes32 requestId, bytes memory bytesData) public recordChainlinkFulfillment(requestId) {
+        emit GetResponse(requestId, bytesData);
+        data = bytesData;
+        rankUrl = string(data);
     }
+
+
+    //    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId) {
+    //        emit RequestVolume(_requestId, _volume);
+    //        volume = _volume;
+    //    }
 
     function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
         uint8 i = 0;
-        while(i < 32 && _bytes32[i] != 0) {
+        while (i < 32 && _bytes32[i] != 0) {
             i++;
         }
         bytes memory bytesArray = new bytes(i);
